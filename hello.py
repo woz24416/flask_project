e4ed7c152e3e0083c47a06f021782adc33602dbb
woz24416@gmail.com <mark@marks-MacBook-Air.local>
2015-06-16 03:43:30 +0800

    Flask Web Development 2b

--- hello.py
+++ hello.py
@@ -11,5 +11,15 @@ app = Flask(__name__) #程序实例是Flask类的对象
 def index():
 	return '<h1>Hello World!</h1>'
 
+@app.route('/user/<name>') #尖括号中的内容就是动态部分,任何能匹配静态部分的 URL 都会映射到这个路由上 
+def user(name): #调用视图函数时,Flask会将动态部分作为参数传入函数 
+	return '<h1>Hello, %s!</h1>' % name
+
 if __name__ == '__main__':
 	app.run(debug=True)
+'''
+__name__=='__main__' 是 Python 的惯常用法
+在这里确保直接执行这个脚本时才启动开发 Web 服务器
+如果这个脚本由其他脚本引入,程序假定父级脚本会启动不同的服务器,因此不会执行 app.run()。 
+
+'''
